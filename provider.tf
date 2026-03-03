0676031f585c9e0fafdef68cb761c5921f5b1057
Daniel Ferreira <danenoisturbera@gmail.com>
2026-03-03 00:16:34 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "framedrop-infra-2"
+    bucket = "framedrop-infra-3"
     key    = "terraform/infra/database/state.tfstate"
     region = "us-east-1"
   }
